e7c93812759e76c4b71a9657f570d8896647f2f8
hpyc098 <hanjunhao098@gmail.com>
2025-11-23 16:34:33 +1300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
 from app import app
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5002))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5002)
